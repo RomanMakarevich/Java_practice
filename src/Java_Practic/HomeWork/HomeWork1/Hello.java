351569c3ecfb6b43be076e5d582fe01ba889124c
RomanMakarevich <romanmak955@gmail.com>
2019-04-17 15:48:48 +0300

    Commit2

---
+++ src/Java_Practic/HomeWork/HomeWork1/Hello.java
@@ -0,0 +1,19 @@
+﻿import java.io.Console;
+
+public class Hello{
+
+int a;
+float b;
+static String age = "";
+
+public static void main (String args[])
+{
+  //заводи переменные там, где ты их будешь использовать, т.е. String age = con...;
+String age = con.readLine("Write your age: ");
+Console con = System.console();
+  //чтобы не было проблем с кодировкой лучше использовать англ язык
+
+System.out.println("You're - " + age);
+
+}
+}
