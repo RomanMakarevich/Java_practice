d3787bb1157f51b84f130fd0a85ee85a7c38a9b3
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-23 21:23:54 +0300

    Update Task_4.java

--- src/java_ractic/homework/homework_3/task_4/Task_4.java
+++ src/java_ractic/homework/homework_3/task_4/Task_4.java
@@ -3,6 +3,8 @@ package java_ractic.homework.homework_3.task_4;
 public class Task_4 {
     public static void main(String[] args) {
 
+        // все гуд, но такие длинные математические выражение все же лучше отдельной строкой
+        // отдельное молодец за структуру пакетов
         System.out.println("(COS(180)+256/8-2^3)*pi = " + ((Math.cos(180) + (256 / 8) - Math.pow(2,3)) * Math.PI));
     }
 }
