c4587bd8cff7d87a301eb8a613c2072ef351a2ea
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-28 23:31:14 +0300

    Update Task_4.java

--- src/java_practic/homework/homework_5/task_4/Task_4.java
+++ src/java_practic/homework/homework_5/task_4/Task_4.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Task_4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //не стесняйся ставить новый абзац, так код гораздо лучше читается
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
+        
         System.out.println("Enter your age:");
         int age = scanner.nextInt();
+        
         System.out.println("Enter year, when you was born:");
         int year = scanner.nextInt();
+        
         System.out.println("Enter your area:");
         String area = scanner.nextLine(); //я не понимаю, почему с переменной "name" scanner.nextLine сработала, а тут эту процедуру просто пропускает.
         myMetod(name, age, year, area);
