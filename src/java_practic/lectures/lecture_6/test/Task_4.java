16eb50888d7bd8a865eb75086212aa4c2198387f
RomanMakarevich <romanmak955@gmail.com>
2019-04-28 17:43:23 +0300

    Commit9

--- src/java_practic/lectures/lecture_6/test/Task_4.java
+++ src/java_practic/lectures/lecture_6/test/Task_4.java
@@ -1,7 +1,27 @@
 package java_practic.lectures.lecture_6.test;
 
+import java.util.Random;
+
 public class Task_4 {
     public static void main(String[] args) {
+        int  [][] array = new int[5][];
+        Random rand = new Random();
+        int min = 1;
+        int max = 5;
+        for (int i = 0; i < array.length; i++){
+            array[i] = new int [rand.nextInt((max - min) + 1) + min];
+        }
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                array[i][j] = rand.nextInt((max - min) + 1) + min;
+            }
+        }
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                System.out.print(array[i][j] + ", ");
+            }
+            System.out.println();
+        }
 
     }
 }
