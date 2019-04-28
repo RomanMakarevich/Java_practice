d1a109bb950a0e3a74ca9698dfa9bec10abddfcb
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-28 23:06:31 +0300

    Update Task_2.java

--- src/java_practic/homework/homework_5/task_2/Task_2.java
+++ src/java_practic/homework/homework_5/task_2/Task_2.java
@@ -6,17 +6,20 @@ public class Task_2 {
     public static void main(String[] args) {
         String[] array = new String[5];
         System.out.println("Enter 5 words:");
+        
         Scanner scanner = new Scanner(System.in);
+        
         for (int i = 0; i < array.length; i++){
             System.out.printf("Word №%d:", i+1);
             array[i] = scanner.nextLine();
         }
         System.out.println(find(array));
-
+// не должно быть здесь пробела
     }
 
 
     static Boolean find (String[] array){
+        // слово dog лучше тоже передавать через аргумент
         String dog ="dog";
         for (int i = 0; i < array.length; i++){
             if (array[i].equals(dog)){
@@ -24,7 +27,7 @@ public class Task_2 {
             }
         }
         return false;
-
+// не должно быть здесь пробела
     }
-
+// не должно быть здесь пробела
 }
