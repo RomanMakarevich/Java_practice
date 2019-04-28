322d01466f34e14997eed4cc840141731a0ec176
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-28 23:13:13 +0300

    Update Task_3.java

--- src/java_practic/homework/homework_5/task_3/Task_3.java
+++ src/java_practic/homework/homework_5/task_3/Task_3.java
@@ -4,14 +4,19 @@ public class Task_3 {
     public static void main(String[] args) {
         int number = 0;
 
+        // как-то не очень отформатировано, используй меню code(вверху)->reformat code
 recursion(number);
     }
 
+    // int замени на void, зачем нам что-то возвращать в данном задании?
     static int recursion (int number){
 
+        // массив надо передавать аргументом
         int[] array = {1,25,465,32,100};
 
         if (number < array.length){
+            // 1, 25, 465, 32, 100, 
+            // это твой вывод, где-то есть лишняя запятая. Надо убрать
             System.out.print(array[number] + ", ");
             return recursion(number +1);
         }
@@ -19,5 +24,5 @@ recursion(number);
             return 0;
         }
     }
-
+// пустую строку надо убрать
 }
