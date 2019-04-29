74eb133cdd46ca7978b5a8c144dcd99cabb8506f
RomanMakarevich <romanmak955@gmail.com>
2019-04-29 22:09:30 +0300

    Commit12

---
+++ src/java_practic/lectures/lecture_7/task_1/Task_1.java
@@ -0,0 +1,19 @@
+package java_practic.lectures.lecture_7.task_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+        Book luna = new Book("Luna", "Pushkin", 1912);
+        String name = luna.getName();
+        String author = luna.getAuthor();
+        int year = luna.getYear();
+        System.out.printf("%s, %s, %d", name, author, year);
+
+        Book sun = new Book(1236);
+        //String sunName = sun.getName();
+        //System.out.println("\n"+sunName);
+        String sunName = sun.setName("Sun");
+        System.out.printf("\n" + sunName);
+
+    }
+}
