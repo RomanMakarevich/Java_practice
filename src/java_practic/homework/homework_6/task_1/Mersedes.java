3722410e42a3ea545456dad2f0e2d1956bb1e7e9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-10 13:18:20 +0300

    Update Mersedes.java

--- src/java_practic/homework/homework_6/task_1/Mersedes.java
+++ src/java_practic/homework/homework_6/task_1/Mersedes.java
@@ -6,6 +6,10 @@ public class Mersedes {
         Transmission mersedesTransmission = new Transmission();
         Car mersedes = new Car("Mersedes", mersedesEengine, mersedesTransmission);
 
+        //  ты сначала вызываешь метод gasPedalOn(), а затем engineOn(). Думаю должно быть наоборот
+        // далее, скорее всего метод для увеличения скорости долже называться speedUp(), а не gasPedalOn()
+        // в целом неплохо, попробуй сделать интерактивное дествие с пользователем,
+        // т.е. через Scanner получать команды, для увеличения скорости, остановки и т.д.
         mersedes.gasPedalOn();
         mersedesEengine.engineOn();
 
