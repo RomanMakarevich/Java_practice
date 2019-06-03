720fa257992f06670b921bc4badbb0e340610644
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-03 14:55:59 +0300

    Update BildingStretComparator.java

--- src/java_practic/homework/homework_11/task_3/BildingStretComparator.java
+++ src/java_practic/homework/homework_11/task_3/BildingStretComparator.java
@@ -2,6 +2,7 @@ package java_practic.homework.homework_11.task_3;
 
 import java.util.Comparator;
 
+//Building; Street - внимательнее
 public class BildingStretComparator implements Comparator<Building> {
     @Override
     public int compare(Building building1, Building building2) {
