e66700138af3962c293ddbc9df71905cf2de3f19
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-18 12:19:14 +0300

    Update Task_1.java

--- src/java_practic/homework/homework_12/task_1/Task_1.java
+++ src/java_practic/homework/homework_12/task_1/Task_1.java
@@ -3,7 +3,9 @@ package java_practic.homework.homework_12.task_1;
 import java.io.*;
 
 public class Task_1 {
+    // private static final String INPUT_FILE_PATH
     private static final String TEXT_PATH = "D:\\Java_Practice\\src\\java_practic\\homework\\homework_12\\task_1\\Text";
+    // private static final String OUTPUT_FILE_PATH
     private static final String TEXT1_PATH = "D:\\Java_Practice\\src\\java_practic\\homework\\homework_12\\task_1\\Text1";
 
     public static void main(String[] args) {
@@ -13,9 +15,11 @@ public class Task_1 {
         try (FileReader fr = new FileReader(TEXT_PATH)) {
             BufferedReader br = new BufferedReader(fr);
 
+            // это все тоже в ресурсы блока try (), иначе надо самому вызывать метод close()
             FileOutputStream fos = new FileOutputStream(TEXT1_PATH);
             PrintStream ps = new PrintStream(fos);
 
+            // у BufferedReader есть метод readline() думаю лучше использовать его
             while ((buf = br.read()) != -1) {
 
                 if (buf == (char) ' ') {
