4c32290f7dab81ba65ae9b165a09fac5c8cf1a7b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-01 17:39:33 +0300

    Update Main.java

--- src/java_practic/lectures/lecture_20/task_5/Main.java
+++ src/java_practic/lectures/lecture_20/task_5/Main.java
@@ -14,18 +14,24 @@ public class Main {
 
         Random random = new Random();
 
+        // i < 10 = и цикл до этого тоже 10. 
+        // sportsmans.size()
         for (int i = 0; i < 10; i++) {
+            // вынеси за пределы цикла. зачем каждый раз создавать переменную
             int maxSpeed = 35;
 
+            // скорость в конструктор и сразу за 1 цикл все сделаешь.
             sportsmans.get(i).setSpeed(random.nextInt(maxSpeed));
         }
         System.out.println(sportsmans);
         System.out.println();
+        // получается, прибежал одинаково с соперником, но имя на "Я" и в итоге медаль ниже рангом? 
         Comparator sportsmanComp = new SpeedComparator().thenComparing(new NameComparator());
 
         TreeSet<Sportsman> sortedSportsmans = new TreeSet<>(sportsmanComp);
         sortedSportsmans.addAll(sportsmans);
 
+        // не стоит так делать с базовой коллекцией. Переопредели метод equals,hashcode и затем бери из листа нужного бегуна и сетай ему медаль
         sportsmans.clear();
         sportsmans.addAll(sortedSportsmans);
         sportsmans.get(0).setMedals(1, Medals.Gold);
