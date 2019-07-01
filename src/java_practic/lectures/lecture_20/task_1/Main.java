725cc0023976f474d2fa897cb1b607fd6bf182c8
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-01 17:25:40 +0300

    Update Main.java

--- src/java_practic/lectures/lecture_20/task_1/Main.java
+++ src/java_practic/lectures/lecture_20/task_1/Main.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
         int number = 5;
+        // Function<Integer, String> func = number -> number + " BYN";
         Function<Integer, String> rubl = number1 -> {
             String result =  number + " BYN";
             return result;
