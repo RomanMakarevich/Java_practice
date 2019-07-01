c1b38f74ba8947df212ac43e8a9faf752b032703
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-01 17:27:40 +0300

    Update UserMain.java

--- src/java_practic/lectures/lecture_20/task_3/UserMain.java
+++ src/java_practic/lectures/lecture_20/task_3/UserMain.java
@@ -2,9 +2,10 @@ package java_practic.lectures.lecture_20.task_3;
 
 public class UserMain {
     public static void main(String[] args) {
-        UserMaker userMaker = User::new ;
+        UserMaker userMaker = User::new;
 
         User mazay = userMaker.make("Mazay");
+        // System.out.println(mazay); <- метод toString() вызовется автоматически
         System.out.println(mazay.toString());
     }
 }
